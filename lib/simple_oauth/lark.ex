1921a2bb3a4bfdfc8af8b9257f6956735fecc06b
Kenton Wang <smartepsh@gmail.com>
2024-01-14 00:12:10 +0800

    feat: lark oauth_url

---
+++ lib/simple_oauth/lark.ex
@@ -0,0 +1,43 @@
+defmodule SimpleOAuth.Lark do
+  @moduledoc """
+  暂只支持使用企业自建应用登录。
+  """
+
+  @behaviour SimpleOAuth.API
+
+  # NOTE https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/authen-v1/login-overview
+
+  @impl true
+  def get_user_info(_code, _config \\ config()) do
+  end
+
+  def oauth_url(config \\ config()) do
+    query = %{
+      app_id: Keyword.fetch!(config, :app_id),
+      redirect_uri: redirect_uri(config),
+      scope: config[:scope],
+      state: config[:state]
+    }
+
+    query =
+      query
+      |> Enum.reject(fn {_key, value} -> is_nil(value) end)
+      |> URI.encode_query(:rfc3986)
+
+    base_url = "https://open.feishu.cn/open-apis/authen/v1/authorize"
+    base_url <> "?" <> query
+  end
+
+  defp redirect_uri(config) do
+    Keyword.fetch!(config, :host) <> config[:callback_path]
+  end
+
+  @doc false
+  def config(config \\ SimpleOAuth.config!(:lark)) do
+    Keyword.merge(default_config(), config)
+  end
+
+  defp default_config do
+    [callback_path: "oauth/lark/callback"]
+  end
+end
