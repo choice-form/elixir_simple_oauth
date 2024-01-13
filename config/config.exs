901735030bc9f4d3427b8c5e0d32907cd5fa234e
Kenton Wang <smartepsh@gmail.com>
2024-01-14 01:05:48 +0800

    chore: import secret.exs config file but ignore it

--- config/config.exs
+++ config/config.exs
@@ -5,3 +5,7 @@ config :tesla, adapter: Tesla.Adapter.Hackney
 if "#{Mix.env()}.exs" |> Path.expand(__DIR__) |> File.exists?() do
   import_config "#{Mix.env()}.exs"
 end
+
+if "#{Mix.env()}.secret.exs" |> Path.expand(__DIR__) |> File.exists?() do
+  import_config "#{Mix.env()}.secret.exs"
+end
