3396e5ede51ca74943a504a2937563d51bfc2f57
Kenton Wang <smartepsh@gmail.com>
2024-09-30 16:13:21 +0800

    feat: replace tesla by using req

--- lib/simple_oauth/lark/client.ex
+++ lib/simple_oauth/lark/client.ex
@@ -1,26 +1,33 @@
 defmodule SimpleOAuth.Lark.Client do
-  use Tesla
-
   defguardp is_2xx(term) when is_integer(term) and term >= 200 and term <= 299
 
   def app_access_token(app_id, app_secret) do
     client()
-    |> post("/auth/v3/app_access_token/internal", %{app_id: app_id, app_secret: app_secret})
+    |> Req.post(
+      url: "/auth/v3/app_access_token/internal",
+      json: %{app_id: app_id, app_secret: app_secret}
+    )
     |> case do
-      {:ok, %{status: status, body: %{"code" => 0} = body}} when is_2xx(status) -> {:ok, body}
-      {:ok, %{body: body}} -> {:error, body}
-      _ -> :error
+      {:ok, %Req.Response{status: status, body: %{"code" => 0} = body}} when is_2xx(status) ->
+        {:ok, body}
+
+      {:ok, %Req.Response{body: body}} ->
+        {:error, body}
+
+      _ ->
+        :error
     end
   end
 
   def token(body, headers) do
     client()
-    |> post("/authen/v1/oidc/access_token", body, headers: headers)
+    |> Req.post(url: "/authen/v1/oidc/access_token", json: body, headers: headers)
     |> case do
-      {:ok, %{status: status, body: %{"code" => 0, "data" => body}}} when is_2xx(status) ->
+      {:ok, %Req.Response{status: status, body: %{"code" => 0, "data" => body}}}
+      when is_2xx(status) ->
         {:ok, body}
 
-      {:ok, %{body: body}} ->
+      {:ok, %Req.Response{body: body}} ->
         {:error, body}
 
       _ ->
@@ -30,12 +37,13 @@ defmodule SimpleOAuth.Lark.Client do
 
   def user_info(token) do
     client()
-    |> get("/authen/v1/user_info", headers: [{"authorization", "Bearer #{token}"}])
+    |> Req.get(url: "/authen/v1/user_info", headers: %{"authorization" => "Bearer #{token}"})
     |> case do
-      {:ok, %{status: status, body: %{"code" => 0, "data" => body}}} when is_2xx(status) ->
+      {:ok, %Req.Response{status: status, body: %{"code" => 0, "data" => body}}}
+      when is_2xx(status) ->
         {:ok, body}
 
-      {:ok, %{body: body}} ->
+      {:ok, %Req.Response{body: body}} ->
         {:error, body}
 
       _ ->
@@ -44,11 +52,6 @@ defmodule SimpleOAuth.Lark.Client do
   end
 
   def client do
-    middleware = [
-      {Tesla.Middleware.BaseUrl, "https://open.feishu.cn/open-apis"},
-      Tesla.Middleware.JSON
-    ]
-
-    Tesla.client(middleware)
+    Req.new(base_url: "https://open.feishu.cn/open-apis", retry: false)
   end
 end
